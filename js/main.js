37076b41f660b265c02d686b1dd1420e5dde88e1
derek.oh93 <derek.oh93@gmail.com>
2017-02-04 17:01:47 -0500

    updated index/js

--- js/main.js
+++ js/main.js
@@ -18,4 +18,54 @@
 // TODO: Customize that Map to show markers with popups at no fewer than 3
 // interesting locations. (You'll need to figure out the latitude/longitude for
 // these locations using a mapping tool such as Google Maps.)
+//doc ready block
+$(document).ready(function(){
+  
+  //maps section
+	//Satelite map layer
+  var satMap = L.tileLayer('https://api.mapbox.com/styles/v1/derekoh93/ciyrrbomg00382rpjuxpr3fu4/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiZGVyZWtvaDkzIiwiYSI6ImNpeXJteWVjdjAwMjYzMnMxM2NsYnZxb2UifQ.WkKLRw3Mk7pEXoSNfW7B3w', {
+    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+    maxZoom: 12,
+    id: 'https://www.mapbox.com/studio/styles/derekoh93/ciyrrbomg00382rpjuxpr3fu4/',
+  });
+	//Street map layer
+  var streetMap = L.tileLayer('https://api.mapbox.com/styles/v1/derekoh93/ciyrru9vv001v2rpt3hk8qewz/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiZGVyZWtvaDkzIiwiYSI6ImNpeXJteWVjdjAwMjYzMnMxM2NsYnZxb2UifQ.WkKLRw3Mk7pEXoSNfW7B3w', {
+    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+    maxZoom: 12,
+    id: 'https://www.mapbox.com/studio/styles/derekoh93/ciyrru9vv001v2rpt3hk8qewz/'
+});
+  //Outdoors map layer
+  var outdoorsMap = L.tileLayer('https://api.mapbox.com/styles/v1/derekoh93/ciyrrw7bs002e2rmne4x0uqk2/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiZGVyZWtvaDkzIiwiYSI6ImNpeXJteWVjdjAwMjYzMnMxM2NsYnZxb2UifQ.WkKLRw3Mk7pEXoSNfW7B3w', {
+    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+	  maxZoom: 12,
+    id: 'https://www.mapbox.com/studio/styles/derekoh93/ciyrrw7bs002e2rmne4x0uqk2/'
+});
+  //Different layers that you can select on the map
+  var baseMap = {
+    "Satellite": satMap,
+    "Streets": streetMap,
+    "Outdoors": outdoorsMap,
+    
+  };
+  //These are the different markers on the map with popup descriptions
+  var paradise = L.marker([46.773421, -121.745159]).bindPopup('Paradise, beautiful view and flower meadows'),
+      longmire = L.marker([46.7502, -121.8128]).bindPopup('Longmire, national historic district'),
+      naradaFalls = L.marker([46.7750, -121.7462]).bindPopup('Narada Falls, beautiful trail hat leads to Narada Falls')
+  //This is the layer group of markers that you can find on the map   
+  var visit = L.layerGroup([paradise,longmire,naradaFalls]);
+  
+  var mapOverlay = {
+      "visit": visit
+  };
+  //Default position of the map when you start the page up     
+  var myMap = L.map('map-container', {
+    center: [46.85, -121.78],
+    zoom: 11,
+    layers: [satMap, visit]
+  });
 
+  L.control.layers(baseMap, mapOverlay).addTo(myMap);
+})
+     
+  
+                  
